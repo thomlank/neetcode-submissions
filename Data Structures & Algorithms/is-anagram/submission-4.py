6fda9dc0d813b024bd7d29053467931175775bd3
thomlank <thomlankford@gmail.com>
2026-04-17 17:42:07 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        # Brute 
+        """
+        if len(s) != len(t):
+            return False
+        s_map = {}
+        t_map = {}
+        for char in s:
+            s_map[char] = s_map.get(char,0)+1
+        for char in t:
+            t_map[char] = t_map.get(char,0)+1    
+        return s_map == t_map
+        """
+
+        freq_key = [0] * 26
+        
+        for char in s:
+            freq_key[ord(char) - ord('a')] += 1
+            
+
+        for char in t:
+            freq_key[ord(char) - ord('a')] -= 1
+        
+        return len(set(freq_key)) == 1    
